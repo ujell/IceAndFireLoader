6060e1655f9b1dd057e38a218e2b3b7d182dfb6a
Yucel Uzun <yuceluzun@windowslive.com>
2016-02-14 15:18:39 +0100

    Added some examples.

--- IceAndFireLoader/main.swift
+++ IceAndFireLoader/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+// Some simple examples;
 
+IceAndFire.load(583) { (character: IceAndFireCharacter?, error) in
+    if error != nil {
+        print (error!)
+    }
+    if character != nil {
+        print (character!.name!)
+    }
+}
+
+IceAndFire.load(1) { (book: IceAndFireBook?, error) in
+    if error != nil {
+        print (error!)
+    }
+    if book != nil {
+        print (book!.name)
+    }
+}
+
+IceAndFire.load(10) { (house: IceAndFireHouse?, error) in
+    if error != nil {
+        print (error!)
+    }
+    if house != nil {
+        print (house!.name!)
+    }
+}
+
+// Main normaly ends without waiting closures to run.
+sleep(50)
